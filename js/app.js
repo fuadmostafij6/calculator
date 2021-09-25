1502a5d511e54de23f540a7f3c7f1f6ca76b6610
webmostafij <fuadmostfij4@gmail.com>
2021-09-25 22:09:25 +0600

    Add existing file

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 let d = document.getElementById('d');
-let r = document.getElementById('r')
-let buttons = Array.from(document.getElementsByClassName('b'));
+let r = document.getElementById('r');
+let b = document.body;
+let l = document.getElementsByTagName('li');
 
+let buttons = Array.from(document.getElementsByClassName('b'));
 
 
 buttons.map( (button) => {
     
     button.addEventListener('click', (e) => {
+        
       
         if( e.target.innerText ==="C"){
             d.innerText = ""
             r.innerText = ""
+           
 
 
         }
@@ -18,6 +22,7 @@ buttons.map( (button) => {
             
             try {
                 r.innerText = eval(d.innerText)
+               
               }
               catch(err) {
                 r.innerText= 'errore'
@@ -27,12 +32,22 @@ buttons.map( (button) => {
         else if(e.target.innerText=== '←'){
            
                 d.innerText = d.innerText.slice(0, -1);
+               
        
         }
         else{
             d.innerText += e.target.innerText;
+         
         }
         
         
     });
 });
+
+function m (){
+
+        b.classList.toggle('lightBody');
+       
+        
+    
+}
